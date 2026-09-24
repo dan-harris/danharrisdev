e2ba1d7009be885c5d84fa5fe0c7d6ac0f0ba544
Dan Harris <danharris@danharris-sws-mbp.local>
2026-09-24 10:50:39 +0930

    ✨ feat: add base Astro app with Cloudflare Pages setup

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://danharris.dev",
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      watch: {
+        usePolling: true,
+      },
+    },
+  },
+  adapter: cloudflare({
+    imageService: "compile",
+  }),
+});
